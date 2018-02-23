79571a54455fedfb35c0b003fb4d8fed44a649cd
Unknown <nevermor9696@gmail.com>
2018-02-23 23:12:27 +0300

    Create .BLL Class Library project and add Identity framework to the .BLL project

---
+++ LoadDistributionForTeachers.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using LoadDistributionForTeachers.BLL.DTO;
+using LoadDistributionForTeachers.BLL.Infrastructure;
+
+namespace LoadDistributionForTeachers.BLL.Interfaces
+{
+    public interface IUserService
+    {
+        Task<OperationDetails> Create(UserDTO userDto);
+        Task<ClaimsIdentity> Authenticate(UserDTO userDto);
+        Task SetInitialData(UserDTO adminDto, List<string> roles);
+    }
+}
---
+++ LoadDistributionForTeachers.BLL/Services/ServiceCreator.cs
@@ -0,0 +1,13 @@
+﻿using LoadDistributionForTeachers.BLL.Interfaces;
+using LoadDistributionForTeachers.DAL.Repositories;
+
+namespace LoadDistributionForTeachers.BLL.Services
+{
+    public class ServiceCreator : IServiceCreator
+    {
+        public IUserService CreateUserService(string connection)
+        {
+            return new UserService(new IdentityUnitOfWork(connection));
+        }
+    }
+}
